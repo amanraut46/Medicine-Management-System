c41f7f6530d624dfd451a43d0854a8358f491adf
amanraut46 <amanraut46@gmail.com>
2026-05-06 10:07:28 +0530

    lazy loading added

--- medicineui/src/App.js
+++ medicineui/src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
-import DisplayMedicine from './DisplayMedicine';
+import React,{Suspense,lazy} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import MedicineDetails from './pages/MedicineDetails';
-import UpdateMedicine from './pages/UpdateMedicine';
-import AddMedicine from './pages/AddMedicine';
+
+const DisplayMedicine = lazy(() => import('./DisplayMedicine'));
+const MedicineDetails = lazy(() => import('./pages/MedicineDetails'));
+const UpdateMedicine = lazy(() => import('./pages/UpdateMedicine'));
+const AddMedicine = lazy(() => import('./pages/AddMedicine'));
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DisplayMedicine />} />
-        <Route path="/medicine/:id" element={<MedicineDetails />} />
-        <Route path="/updatemedicine/:id" element={<UpdateMedicine />} />
-        <Route path="/addmedicine" element={<AddMedicine />} />
-      </Routes>
-    </Router>
+    <Suspense fallback="Loading...">
+      <Router>
+        <Routes>
+          <Route path="/" element={<DisplayMedicine />} />
+          <Route path="/medicine/:id" element={<MedicineDetails />} />
+          <Route path="/updatemedicine/:id" element={<UpdateMedicine />} />
+          <Route path="/addmedicine" element={<AddMedicine />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
