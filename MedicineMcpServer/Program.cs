a3f31974c5e6a88409d18afa682856fd7ebef73d
amanraut46 <amanraut46@gmail.com>
2026-05-13 10:07:34 +0530

    program file modified

--- MedicineMcpServer/Program.cs
+++ MedicineMcpServer/Program.cs
@@ -5,13 +5,11 @@ using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHttpClient<MedicineApiService>(client => {
-    client.BaseAddress = new Uri("https://localhost:7292/");
+    client.BaseAddress = new Uri("http://host.docker.internal:7292/");
 });
 
 builder.Services.AddMcpServer()
     .WithStdioServerTransport()
     .WithTools<MedicineTools>();
 
-var app = builder.Build();
-
-app.Run();
+await builder.Build().RunAsync();
